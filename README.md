ebd4df9843648343f463c1b987e151c21e0a037f
Malkist <titan.tanihaha@yahoo.com>
2023-02-25 14:23:18 +0700

    Version 1.1.0
    Added customization
    More keywords
    Updated `README.md`

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # JSAway
 
+![npm](https://img.shields.io/npm/dw/jsaway) ![npm](https://img.shields.io/npm/v/jsaway) ![NPM](https://img.shields.io/npm/l/jsaway) ![GitHub Repo stars](https://img.shields.io/github/stars/MalkistCoder/JSAway)
+
 JS Away is a simple swear filter that was inspired by [GoAway](https://github.com/TwiN/go-away/) by [TwiN](https://github.com/TwiN/).
 
 ## Installation
 
-`npm install --save jsaway`
+Go into your terminal and type:
+
+```
+npm install --save jsaway
+```
 
 ## Usage
 
@@ -27,9 +33,47 @@ profDetector.has('fuck') // true
 profDetector.extract('fuck you.') // 'fuck'
 ```
 
-JSAway also supports basic leetspeak detection (for example, "p00p" will still be detected).
+JSAway also supports basic leetspeak detection (for example, "t3st" will still be detected).
 
 ```js
 profDetector.has('$h1+') // true
 profDetector.extract('i love eating $h1+') // '$h1+'
+```
+
+### Customization
+
+JSAway can be customized to only block certain profanities. You can do this buy either editing the list of default profanities, or by creating your own.
+
+To add and remove profanities from the default profanities (see profanities.json for the full list), you can use the `ProfanityDetector.addProfanity` and `ProfanityDetector.removeProfanity` functions.
+
+```js
+profDetector.addProfanity('test')
+profDetector.has('test') // test
+profDetector.extract('t3s+') // 't3s+'
+
+profDetector.removeProfanity('test')
+profDetector.extract('test') // null
+```
+
+You can also make your entire own list of profanities by simply editing the `ProfanityDetector.profanities` array.
+
+```js
+profDetector.profanities = ['test', 'words']
+profDetector.has('test') // true
+profDetector.extract('t3s+') // 't3s+'
+```
+
+## Examples
+
+```js
+import ProfanityDetector from 'jsaway'
+
+const profDetector = new ProfanityDetector()
+
+let message = 'Hello there!'
+if (!profDetector.has(message)) {
+    console.log(message)
+} else {
+    console.log(`Your message contains blocked words. ${profDetector.extract(message)}`)
+}
 ```
--- index.js
+++ index.js
@@ -1,27 +1,60 @@
 import defaultProfanities from './profanities.json' assert {type: "json"}
 import replaceChars from './replaceChars.json' assert {type: "json"}
 
+function formatProfanity(profanity) {
+    const regexFormatted = profanity.split('').reduce((word, letter) => {
+        if (replaceChars[letter] === undefined) {
+            return word+letter
+        } else {
+            return word+`[${replaceChars[letter].join('')}${letter}]`
+        }
+    }, '')
+
+    return new RegExp('\\b' + regexFormatted + '\\b', 'i')
+}
+
 class ProfanityDetector {
+    #profanities = []
+
     constructor(profanities=defaultProfanities) {
-        this.profanities = profanities.map(profanity => {
-            const regexFormatted = profanity.split('').reduce((word, letter) => {
-                if (replaceChars[letter] === undefined) {
-                    return word+letter
-                } else {
-                    return word+`[${replaceChars[letter].join('')}${letter}]`
-                }
-            }, '')
-            return new RegExp(regexFormatted, 'i')
-        })
+        this.#profanities = profanities.map(profanity => formatProfanity(profanity))
+    }
+
+    get profanities() {
+        return this.#profanities
+    }
+
+    set profanities(profanities) {
+        this.#profanities = profanities.map(profanity => formatProfanity(profanity))
+    }
+
+    addProfanity(profanity) {
+        this.#profanities.push(formatProfanity(profanity))
+        return profanity
+    }
+
+    removeProfanity(profanity) {
+        const profanitiesStrings = this.#profanities.map(profanity => String(profanity))
+        const formattedProfanity = String(formatProfanity(profanity))
+
+        const index = profanitiesStrings.indexOf(formattedProfanity)
+        if (index < 0) {
+            return null
+        }
+
+        this.#profanities.splice(index, 1)
+        return profanity
     }
 
     extract(string) {
-        for (const regexp of this.profanities) {
+        for (const regexp of this.#profanities) {
             const regexpResult = regexp.exec(string)
+
             if (regexpResult) {
                 return regexpResult[0]
             }
         }
+
         return null
     }
 
